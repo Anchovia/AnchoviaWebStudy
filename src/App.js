12ced1fab6afa4bb04cfe3b24b404ca7eb09a5f5
Anchovia <hodolye@naver.com>
2023-01-15 16:36:46 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import Day from "./component/Day";
 import DayList from "./component/DayList";
 import Header from "./component/Header";
+import EmptyPage from "./component/EmptyPage";
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <DayList/>
-      <Day/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+        <Routes>
+          <Route path="/" element={<DayList/>}/>
+          <Route path="/day/:day" element={<Day/>}/>
+          <Route path="/*" element={<EmptyPage/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/component/Day.js
+++ src/component/Day.js
@@ -1,20 +1,23 @@
 import dummy from "../db/data.json"
+import { useParams } from "react-router-dom";
 
 export default function Day(){
-    const day = 1;
-    const wordList = dummy.words.filter(word => (word.day === day));
+    const { day } = useParams();
+    const wordList = dummy.words.filter(word => (word.day === Number(day)));
 
     return(
-         
-        <table>
-            <tbody>
-                {wordList.map(word=>(
-                    <tr key={word.id}>
-                        <td>{word.eng}</td>
-                        <td>{word.kor}</td>
-                    </tr>
-                ))}
-            </tbody>
-        </table>
+        <>
+            <h2>Day {day}</h2>
+            <table>
+                <tbody>
+                    {wordList.map(word=>(
+                        <tr key={word.id}>
+                            <td>{word.eng}</td>
+                            <td>{word.kor}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
     );
 }
