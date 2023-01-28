ae47a1c17a282c8d56a6283adb0d8cb0223be45f
Anchovia <hodolye@naver.com>
2023-01-28 16:59:53 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-
-import Day from "./component/Day";
-import DayList from "./component/DayList";
-import Header from "./component/Header";
-import EmptyPage from "./component/EmptyPage";
-import CreateWord from './component/CreateWord';
-import CreateDay from './component/CreateDay';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [posts, setPosts] = useState("남자 코트 추천");
+
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Header/>
-        <Routes>
-          <Route path="/" element={<DayList/>}/>
-          <Route path="/day/:day" element={<Day/>}/>
-          <Route path="/create_word" element={<CreateWord/>}/>
-          <Route path="/create_day" element={<CreateDay/>}/>
-          <Route path="/*" element={<EmptyPage/>}/>
-        </Routes>
+    <div className="App">
+      <div className="black-nav">
+        <div>개발 Blog</div>
       </div>
-    </BrowserRouter>
+      <div className="list">
+        <h3>{ posts }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+    </div>
   );
 }
 
---
+++ src/backUpApp.js
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Day from "./component/Day";
+import DayList from "./component/DayList";
+import Header from "./component/Header";
+import EmptyPage from "./component/EmptyPage";
+import CreateWord from './component/CreateWord';
+import CreateDay from './component/CreateDay';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+        <Routes>
+          <Route path="/2" element={<DayList/>}/>
+          <Route path="/day/:day" element={<Day/>}/>
+          <Route path="/create_word" element={<CreateWord/>}/>
+          <Route path="/create_day" element={<CreateDay/>}/>
+          <Route path="/*" element={<EmptyPage/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
