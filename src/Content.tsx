5d240d6f21de42fcdfcea15c5bf9a6f763482b8b
Anchovia <hodolye@naver.com>
2024-02-21 17:07:25 +0900

    C6

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,7 +1,30 @@
+// C6
+
 const Content = () => {
-    return(
+    const handleNameChange = () => {
+        const names = ['A', 'B', 'C'];
+        const int = Math.floor(Math.random() * 3);
+        return names[int];
+    };
+
+    const handleClick = () => {
+        console.log('1')
+    }
+
+    const handleClick2 = (name : string) => {
+        console.log(`props : ${name}`)
+    }
+
+    const handleClick3 = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        console.log(e);
+    }
+
+    return (
         <main>
-            
+            <p onDoubleClick={handleClick}>Hello {handleNameChange()}</p>
+            <button onClick={handleClick}>Click</button>
+            <button onClick={() => {handleClick2('apple')}}>Click</button>
+            <button onClick={(e) => {handleClick3(e)}}>Click</button>
         </main>
     );
 };
